8f2c995213d48e6f6b7a711ba4dba4bd10adf53f
Andres <ahernandez@jjbcompu-innovation.com>
2016-05-25 19:44:35 -0430

    controllers: deleterRole

--- app/controllers/role.py
+++ app/controllers/role.py
@@ -16,15 +16,12 @@ class role():
 		check_role_name = type(role_name) == str
 		
 		if (check_role_name):
-
 			check_long_role_name= CONST_MIN_ROLE_NAME <= len(role_name) <= CONST_MAX_ROLE_NAME
 
 			if (check_long_role_name):
-				
 				check_if_exist = Role.query.filter_by(role_name=role_name).first()
 
 				if (check_if_exist != []):
-
 					new_role = Role(role_name)
 					db.session.add(new_role)
 					db.session.commit()
@@ -38,7 +35,6 @@ class role():
 		check_long_role_name= CONST_MIN_ROLE_NAME <= len(role_name) <= CONST_MAX_ROLE_NAME
 		
 		if (check_target_id and check_long_target_id and check_role_name and check_long_role_name):
-
 			role = Role.query.filter_by(id=target_id).first()
 			
 			if (role != []):
@@ -48,8 +44,20 @@ class role():
 				return True
 		return False
 
-	def deleteRole():
-		pass
+	def deleteRole(self, target_id, role_name):
+		check_target_id 	= type(check_target_id) == int
+		check_long_target_id= CONST_MIN_ID <= target_id <= CONST_MAX_ID
+		check_role_name 	= type(role_name) == str
+		check_long_role_name= CONST_MIN_ROLE_NAME <= len(role_name) <= CONST_MAX_ROLE_NAME
+
+		if (check_target_id and check_long_target_id and check_role_name and check_long_role_name):
+			role = Role.query.filter_by(id=target_id).first()
+
+			if (role != []):
+				db.session.delete(role)
+				db.session.commit()
+				return True
+		return False
 
 	def getRole(self, target_id):
 		check_target_id = type(check_target_id) == int
