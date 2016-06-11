a77cb1288ece366f75a7f4d08558dc49e33bf4a4
Genessis <genessis.ksc@gmail.com>
2016-06-11 00:13:45 -0430

    Pruebas para Profile terminadas

--- test/testProfile.py
+++ test/testProfile.py
@@ -56,8 +56,8 @@ class TestProfile(unittest.TestCase):
 		result = profile_controller.insertProfile(123, 'Femenino', fecha, 5468, '04242135767', -2154)
 		self.assertFalse(result)
 	##################################     
-  	#   Pruebas para updateProfile   #
- 	##################################
+	#   Pruebas para updateProfile   #
+	##################################
 	def testUpdateProfileTrue(self):
 		fecha = date(2016, 7, 1)
 		profile_controller = profile()
@@ -91,6 +91,102 @@ class TestProfile(unittest.TestCase):
 		p = profile_controller.getProfile(123)
 		profile_controller.deleteProfile(p.id)
 		self.assertFalse(result)
+	def testUpdateProfileStatusNotStr(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(123, 'Femenino', fecha, 575, '04242135767', 'direccion')
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	def testUpdateProfileTelphoneNotStr(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(123, 'Femenino', fecha, 'Soltero', 4242135767, 'direccion')
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	def testUpdateProfileAddressNotStr(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', -8)
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	def testUpdateProfileCIMAX(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(1000000000, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	def testUpdateProfileSexStrMax(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(123, 'Femeninoooo', fecha, 'Soltero', '04242135767', 'direccion')
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	def testUpdateProfileStatusStrMax(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(123, 'Femenino', fecha, 'Solteroooooooooo', '04242135767', 'direccion')
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	def testUpdateProfileTelphoneStrMax(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(123, 'Femenino', fecha, 'Soltero', '0042421357670000', 'direccion')
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	def testUpdateProfileAddressStrMax(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.updateProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', "d" * 101)
+		p = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(p.id)
+		self.assertFalse(result)
+	##################################     
+	#   Pruebas para deleteProfile   #
+	##################################
+	def testDeleteProfileTrue(self):
+		fecha = date(2016, 7, 1)
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		p = profile_controller.getProfile(123)
+		result = profile_controller.deleteProfile(p.id)	
+		self.assertTrue(result)
+	def testDeleteProfileFalse(self):
+		profile_controller = profile()
+		result = profile_controller.deleteProfile('123')
+		self.assertFalse(result)
+	###############################     
+	#   Pruebas para getProfile   #
+	###############################
+	def testGetProfileTrue(self):
+		fecha = date(2016, 7, 1)	
+		profile_controller = profile()
+		profile_controller.insertProfile(123, 'Femenino', fecha, 'Soltero', '04242135767', 'direccion')
+		result = profile_controller.getProfile(123)
+		profile_controller.deleteProfile(result.id)
+		self.assertEqual(123, result.ci_user)
+	def testGetProfileDoNotExists(self):
+		profile_controller = profile()
+		result = profile_controller.getProfile(0)
+		self.assertEqual([], result)
+	def testGetProfileNotValidID(self):
+		profile_controller = profile()
+		result = profile_controller.getProfile('2')
+		self.assertEqual([],result)
 
 if __name__ == '__main__':
 	unittest.main()
